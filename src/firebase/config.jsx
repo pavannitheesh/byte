29db37326ed0f39d17040014d149d872f39ee0ea
pavannitheesh <gpavannitheesh1@gmail.com>
2024-03-18 23:16:49 +0530

    first commit

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,21 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Web app's Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCAYOYDuMKGGjTSJL5uDzG5hjQ6y_vYPiI",
-  authDomain: "auction-website-b12fc.firebaseapp.com",
-  databaseURL: "https://auction-website-b12fc.firebaseio.com",
-  projectId: "auction-website-b12fc",
-  storageBucket: "auction-website-b12fc.appspot.com",
-  messagingSenderId: "791747024664",
-  appId: "1:791747024664:web:215a222a81c6d0c2aeb06d",
+  apiKey: "AIzaSyCrZrED2AvxGv4i2ZrfwrwmFZ-FSE5wgyo",
+  authDomain: "byteauction.firebaseapp.com",
+  projectId: "byteauction",
+  storageBucket: "byteauction.appspot.com",
+  messagingSenderId: "957279062730",
+  appId: "1:957279062730:web:142c3463df69a9058a0233",
+  measurementId: "G-KNV1K7VYWQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore and Auth
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
